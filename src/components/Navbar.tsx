cc72d6e011002ec709f4f1ee179aec45fa0823d0
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-04-28 12:16:44 +0100

    Initial commit Project setup and depenecy configurations

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+
+
+
+
+export default function Navbar() {
+	return (
+		<header className="p-4 shadow-sm">
+			<h1 className="text-xl text-green-800 drop-shadow-lg">gt.Express</h1>
+		</header>
+	)
+}
